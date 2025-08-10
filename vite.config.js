887d62938e7aac6e643005da244c7d8f3f8e7ead
Shashank Verma <shashankverma@Shashanks-MacBook-Air.local>
2025-08-10 16:32:47 -0500

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Make sure this matches your project's structure
+      "@": path.resolve(__dirname, "./src"), // Make sure this matches your project's structure
     },
   },
-  base: "/portfolio/"
-})
-
+  base: "/myportfolio/",
+});
